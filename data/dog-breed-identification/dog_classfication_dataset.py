a7839b3709dd3c8edad85d5d7a6e376f6b2a0967
jiawenduo <13127103609@163.com>
2024-05-25 18:47:35 +0800

    修改

---
+++ data/dog-breed-identification/dog_classfication_dataset.py
@@ -0,0 +1,129 @@
+# 小狗分类数据集处理
+import pandas as pd
+import cv2 as cv
+import os
+import glob
+import random
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torch
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+NUM_CATEGORY = 120
+VAL_COUNT_PER_CATEGORY = 66
+
+transform = {
+    "train": torchvision.transforms.Compose([
+        # 随机裁剪图像，所得图像为原始面积的0.08～1之间，高宽比在3/4和4/3之间。
+        # 然后，缩放图像以创建224x224的新图像
+        torchvision.transforms.RandomResizedCrop(224, scale=(0.08, 1.0),
+                                                 ratio=(3.0 / 4.0, 4.0 / 3.0)),
+        torchvision.transforms.RandomHorizontalFlip(),
+        # 随机更改亮度，对比度和饱和度
+        torchvision.transforms.ColorJitter(brightness=0.4,
+                                           contrast=0.4,
+                                           saturation=0.4),
+        # 添加随机噪声
+        torchvision.transforms.ToTensor(),
+        # 标准化图像的每个通道
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                         [0.229, 0.224, 0.225])]),
+    "test": torchvision.transforms.Compose([
+        torchvision.transforms.Resize(256),
+        # 从图像中心裁切224x224大小的图片
+        torchvision.transforms.CenterCrop(224),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                         [0.229, 0.224, 0.225])])
+}
+
+
+def get_csv(path):
+    """
+    获取csv文件
+    :param path:
+    :return:
+    """
+    df = pd.read_csv(path)
+    return df
+
+
+def get_all_category(df):
+    """
+    获取类别名与位序的映射
+    :param df:
+    :return:
+    """
+    category = df['breed'].unique()
+    breed2id = {}
+    id2breed = {}
+
+    for i in range(len(category)):
+        breed2id[category[i]] = i
+        id2breed[i] = category[i]
+
+    return breed2id, id2breed
+
+
+def get_all_image(df, breed2id, id2breed):
+    """
+    获取所有图片地址与id的映射
+    :param df:
+    :param breed2id:
+    :param id2breed:
+    :return:
+    """
+    record = df.values.tolist()
+    random.shuffle(record)
+    record = [[os.path.join(ROOT, 'train', i[0]), breed2id[i[1]]] for i in record]
+    img2id = dict(record)
+
+    return img2id
+
+
+def split_train_test(img2id, val_rate=0.1):
+    train_img2id = {}
+    val_img2id = {}
+
+    tag = [0] * NUM_CATEGORY
+    count = val_rate * VAL_COUNT_PER_CATEGORY
+
+    for i, v in img2id.items():
+        if tag[v] < count:
+            val_img2id[i] = v
+            tag[v] += 1
+        else:
+            train_img2id[i] = v
+    return train_img2id, val_img2id
+
+
+class DogDataset(Dataset):
+    def __init__(self, img2id, is_train=True, transform=None):
+        self.img2id = img2id
+        self.is_train = is_train
+        if is_train:
+            self.transform = transform['train']
+        else:
+            self.transform = transform['test']
+
+    def __getitem__(self, index):
+        img_path = self.img2id[index].key()
+        label = self.img2id[index].value()
+        img = Image.open(img_path)
+        img = self.transform(img)
+        if not self.is_train:
+            return img
+        return img, torch.tensor(label)
+
+
+def get_dataloader(train_dataset, val_dataset, batch_size):
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    return train_loader, val_loader
+
+
+if __name__ == '__main__':
+    pd = get_csv(os.path.join(ROOT, 'labels.csv'))
+    breed2id, id2breed = get_all_category(pd)
+    ing2id = get_all_image(pd, breed2id, id2breed)
+    split_train_test(ing2id)
